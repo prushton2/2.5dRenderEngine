7b1690be56dc446529d91354b4adee98a5c3bac9
prushton2 <peterrushton418@gmail.com>
2021-09-21 14:11:46 -0400

    added objects and main code

--- main.py
+++ main.py
@@ -3,10 +3,44 @@ from DistanceCalculator import *
 from Object import *
 from Render import *
 
+Objects = [
+    Object(Vector2(3, 3), [
+        Side(Vector2(1, 2), Vector2(1,4)),
+        Side(Vector2(5, 4), Vector2(1,4)),
+        Side(Vector2(5, 4), Vector2(5,2)),
+        Side(Vector2(1, 2), Vector2(5,2))
+    ]),
+
+    Object(Vector2(0, 5), [
+        Side(Vector2(-1, 4), Vector2(-1,6)),
+        Side(Vector2(1, 6), Vector2(-1,6)),
+        Side(Vector2(1, 6), Vector2(-1,6)),
+        Side(Vector2(-1, 4), Vector2(-1,6))
+    ])
+]
+
+FOV = (-1, 1)
 
 def main():
-    print("starting")
+    pass
+    linesToRender = []
+    for i in Objects:
+        linesToRender += (i.getSidesInFOV(FOV))
+    # print(linesToRender)
+    
+    pointsToRender = []
+    for i in linesToRender:
+        pointsToRender += i.getAllPoints(10)
+    # print("----------------------")
+    # print(pointsToRender)
 
+    renderPoints = DistanceCalculator.getAllDistances(pointsToRender)
+
+    # print("-----------------------")
+    # print(renderPoints)
+
+    renderer = Render(renderPoints)
+    renderer.render()
 
 
 
