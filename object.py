5849596e7880275de9278fd6a6fbe61d2eeabd40
prushton2 <peterrushton418@gmail.com>
2021-09-21 09:26:14 -0400

    added side class

--- object.py
+++ object.py
@@ -1,3 +1,52 @@
 #This will be for objects that need to be rendered
+v = __import__("vector2")
+class Side:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def getAllPoints(self):
+        x1, y1 = self.point1.x, self.point1.y
+        x2, y2 = self.point2.x, self.point2.y
+        points = []
+        issteep = abs(y2-y1) > abs(x2-x1)
+        if issteep:
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
+        rev = False
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+            rev = True
+        deltax = x2 - x1
+        deltay = abs(y2-y1)
+        error = int(deltax / 2)
+        y = y1
+        ystep = None
+        if y1 < y2:
+            ystep = 1
+        else:
+            ystep = -1
+        for x in range(x1, x2 + 1):
+            if issteep:
+                points.append((y, x))
+            else:
+                points.append((x, y))
+            error -= deltay
+            if error < 0:
+                y += ystep
+                error += deltax
+        # Reverse the list if the coordinates were reversed
+        if rev:
+            points.reverse()
+        return points
+
+
 class Object:
-    def __init__(self, center)
+    def __init__(self, center, sides):
+        center = self.center
+        sides = self.sides
+
+
+s1 = Side(v.Vector2(1, 0), v.Vector2(2, 4))
+print(s1.getAllPoints())
