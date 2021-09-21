2361a08aa79f058b67c4eeda4430a9c8e2d2972b
prushton2 <peterrushton418@gmail.com>
2021-09-21 13:18:22 -0400

    getallpoints returns an array of vectors

--- Object.py
+++ Object.py
@@ -1,4 +1,6 @@
 #This will be for objects that need to be rendered
+from Vector2 import *
+
 class Side:
     def __init__(self, point1, point2): #point1 & 2 should be a vector2
         self.point1 = point1
@@ -31,9 +33,9 @@ class Side:
             ystep = -1
         for x in range(x1, x2 + 1):
             if issteep:
-                points.append((y, x))
+                points.append(Vector2(y, x))
             else:
-                points.append((x, y))
+                points.append(Vector2(x, y))
             error -= deltay
             if error < 0:
                 y += ystep
@@ -49,8 +51,10 @@ class Object:
         self.center = center
         self.sides = sides
     
-    def getSidesInFOV(self, slope1, slope2): #sloped are the slopes of the edge of the FOV. FOV will naturally be 90, slopes should be -1 and 1
+    def getSidesInFOV(self, FOV): #sloped are the slopes of the edge of the FOV. FOV will naturally be 90, slopes should be -1 and 1
         sidesToRender = []
+        slope1 = FOV[0]
+        slope1 = FOV[1]
         for i in self.sides:
             if(i.point1.y > slope1 * i.point1.x and i.point1.y > slope1 * i.point1.x):
                 sidesToRender.append(i)
