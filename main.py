ccd3687c83e000f9f861e5951f63e3a3e5b6a9d1
prushton2 <peterrushton52@gmail.com>
2021-09-21 21:29:33 -0400

    added janky movement

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Vector2 import *
 from DistanceCalculator import *
 from Object import *
 from Render import *
+from tkinter import *
+
 
 Objects = [
     Object([
@@ -18,20 +20,40 @@ Objects = [
         Side(Vector2(-3, 4), Vector2(-1,4))
     ])
 ]
-
+cameraPos = Vector2(0, 0)
 FOV = (-1, 1)
 
 def main():
-    pass
+
+    root = Tk()
+    root.geometry('300x200')
+    text = Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
+    text.pack()
+    root.bind('<KeyPress>', renderScreen)
+    root.mainloop()
+
+def renderScreen(event):
+    global cameraPos
+    print(event.char)
+
+    if(event.char == "w"):
+        cameraPos += Vector2(0, 1)
+    elif(event.char == "a"):
+        cameraPos += Vector2(-1, 0)
+    elif(event.char == "s"):
+        cameraPos += Vector2(0, -1)
+    elif(event.char == "d"):
+        cameraPos += Vector2(1, 0)
+
     linesToRender = []
     for i in Objects:
         linesToRender += (i.getSidesInFOV(FOV))
     
     pointsToRender = []
     for i in linesToRender:
-        pointsToRender += i.getAllPoints(100)
+        pointsToRender += i.getAllPoints(500)
 
-    renderPoints = DistanceCalculator.getAllDistances(pointsToRender)
+    renderPoints = DistanceCalculator.getAllDistances(pointsToRender, cameraPos)
 
 
     renderer = Render(renderPoints)
