6c96de630bfa084abe659e7648969c2d08c0b53b
prushton2 <peterrushton52@gmail.com>
2021-09-25 19:13:40 -0400

    returns distance, needs to return angle

--- DistanceCalculator.py
+++ DistanceCalculator.py
@@ -1,12 +1,19 @@
 from Vector2 import *
+import math
 # from Object import *
 
 class DistanceCalculator:
 
     
     @staticmethod
-    def getDistanceToCamera(camera, point): 
-        pass
+    def getDistanceToCamera(camera, point):
+
+        sideA = math.abs(camera.pos.x - point.x)
+        sideB = math.abs(camera.pos.y - point.y)
+        sideC = (sideA**2 + sideB**2)**.5
+
+        
+
     @staticmethod
     def getAllDistances(camera, points): 
         pass
