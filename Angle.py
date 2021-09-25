cb561a91a3229223fe9807d076f2875f83b3e839
prushton2 <peterrushton52@gmail.com>
2021-09-25 18:05:15 -0400

    Added gt and lt to angle. testing gt or lt will tell if the given angle is within 180 degrees + or - of the other angle

--- Angle.py
+++ Angle.py
@@ -7,16 +7,38 @@ class Angle():
         return str(self.angle)
 
     def __add__(self, other):
-        return (self.angle + other.angle) % 360
+        return Angle((self.angle + other.angle) % 360)
     
     def __sub__(self, other):
-        return (self.angle - other.angle) % 360
+        return Angle((self.angle - other.angle) % 360)
     
     def __mul__(self, other):
-        return (self.angle * other.angle) % 360
+        return Angle((self.angle * other.angle) % 360)
     
     def __truediv__(self, other):
-        return (self.angle / other.angle) % 360
+        return Angle((self.angle / other.angle) % 360)
+
+    def __gt__(self, other):
+        angle = self.angle
+        opposite = self - Angle(180)
+        opposite = opposite.angle
+        
+        if(angle - opposite <= 0):
+            return other.angle > opposite or other.angle < angle
+        else:
+            return other.angle > opposite and other.angle < angle
+
+    
+    def __lt__(self, other):
+        angle = self.angle
+        opposite = self - Angle(180)
+        opposite = opposite.angle
+        
+        if(opposite - angle <= 0):
+            return other.angle < opposite or other.angle > angle
+        else:
+            return other.angle < opposite and other.angle > angle
+
 
     def toPosition(self):
         if (self.angle > 180):
